e6aa1c7e9436fb5388baeb24bc73006042719239
khodchenko <khodch3nk0@gmail.com>
2023-10-26 18:19:42 +0200

    ListItem

--- app/src/main/java/com/khodchenko/waterbalanceapp/MainActivity.kt
+++ app/src/main/java/com/khodchenko/waterbalanceapp/MainActivity.kt
@@ -3,24 +3,71 @@ package com.khodchenko.waterbalanceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.khodchenko.waterbalanceapp.ui.theme.PurpleGrey80
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(Modifier.background(PurpleGrey80).fillMaxSize(),
-                horizontalArrangement = Arrangement.Center) {
-                Text(text = "Hello, World!")
-            }
+
 
         }
+    }
+}
+
+@Preview
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+    ) {
+        Box(
+        ) {
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(shape = CircleShape)
+                )
+                Column (modifier = Modifier.padding(start = 16.dp)){
+                    Text(text = "Léa Seydoux")
+                    Text(text = "French actor")
+                }
+
+            }
+        }
+
+
     }
 }
 
