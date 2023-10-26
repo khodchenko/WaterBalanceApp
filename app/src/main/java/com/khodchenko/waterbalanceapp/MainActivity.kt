44c817f42fba6b2251809581e8adf095e36ac43d
khodchenko <khodch3nk0@gmail.com>
2023-10-27 00:47:07 +0200

    CircleItem()

--- app/src/main/java/com/khodchenko/waterbalanceapp/MainActivity.kt
+++ app/src/main/java/com/khodchenko/waterbalanceapp/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,21 +19,26 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
+            CircleItem()
         }
     }
 }
@@ -41,15 +49,17 @@ private fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .clickable { },
         shape = RoundedCornerShape(15.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 6.dp
         ),
     ) {
+
         Box(
         ) {
-            Row (verticalAlignment = Alignment.CenterVertically){
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
                     painter = painterResource(id = R.drawable.image),
                     contentDescription = "Image",
@@ -59,15 +69,45 @@ private fun ListItem(name: String, prof: String) {
                         .size(64.dp)
                         .clip(shape = CircleShape)
                 )
-                Column (modifier = Modifier.padding(start = 16.dp)){
+                Column(modifier = Modifier.padding(start = 16.dp)) {
                     Text(text = "Léa Seydoux")
                     Text(text = "French actor")
                 }
 
+
             }
         }
 
+    }
+}
 
+@Preview
+@Composable
+private fun CircleItem() {
+    var counter = remember {
+        mutableStateOf(0)
+    }
+    var backgroundColor = remember {
+        mutableStateOf(Color.Cyan)
+    }
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = backgroundColor.value, shape = CircleShape)
+            .clickable {
+                when (++counter.value) {
+                    10 -> backgroundColor.value = Color.Red
+                    20 -> backgroundColor.value = Color.Green
+                }
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            textAlign = TextAlign.Center,
+            style = TextStyle(color = Color.White, fontSize = 20.sp)
+        )
     }
 }
 
