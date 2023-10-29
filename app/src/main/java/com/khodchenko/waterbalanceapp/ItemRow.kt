bb4b7b2ea60d061723f9fe8531a935f4257d6c84
khodchenko <khodch3nk0@gmail.com>
2023-10-29 15:36:06 +0100

    List

--- app/src/main/java/com/khodchenko/waterbalanceapp/ItemRow.kt
+++ app/src/main/java/com/khodchenko/waterbalanceapp/ItemRow.kt
@@ -3,25 +3,37 @@ package com.khodchenko.waterbalanceapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun ItemRow(item: ItemRowModel) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    Row(
         modifier = Modifier
             .background(Color.White)
+            .fillMaxWidth()
             .padding(3.dp)
     )
     {
@@ -32,9 +44,17 @@ fun ItemRow(item: ItemRowModel) {
             modifier = Modifier
                 .padding(3.dp)
                 .clip(CircleShape)
+                .size(60.dp)
         )
-        Text(
-            text = item.title
-        )
+        Column(modifier = Modifier.padding(start = 10.dp, top = 10.dp)) {
+            Text(text = item.title)
+            Text(
+                modifier = Modifier.clickable {
+                    isExpanded = !isExpanded
+                },
+                maxLines = if (isExpanded) 10 else 1,
+                text = item.content
+            )
+        }
     }
 }
--- app/src/main/java/com/khodchenko/waterbalanceapp/ItemRowModel.kt
+++ app/src/main/java/com/khodchenko/waterbalanceapp/ItemRowModel.kt
@@ -2,5 +2,6 @@ package com.khodchenko.waterbalanceapp
 
 data class ItemRowModel(
     val imageId: Int,
-    val title: String
+    val title: String,
+    val content: String
 )
